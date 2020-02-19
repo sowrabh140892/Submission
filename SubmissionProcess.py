91d070af1d51709c6ac6bac6d2f668e673538365
sowrabh140892 <49732417+sowrabh140892@users.noreply.github.com>
2020-02-19 12:21:53 -0800

    Update SubmissionProcess.py

--- SubmissionProcess.py
+++ SubmissionProcess.py
@@ -17,25 +17,24 @@ UAP_SNS_TOPIC="arn:aws:sns:ap-southeast-2:416748369696:UAPSNS"
 
 count=os.environ['AWS_BATCH_JOB_ARRAY_INDEX']
 
-
 BUCKET='aws-batch-parameter'
 
-KEY=str(sys.argv[1])
+
 
 session = boto3.Session(region_name='ap-southeast-2')
 print dir(session)
 
 
 try:
-    s3 = boto3.resource('s3')
-    s3.Bucket(BUCKET).download_file(KEY, '/tmp/%s'%(KEY))
+	s3 = boto3.resource('s3')
+	s3.Bucket(BUCKET).download_file(KEY, '/tmp/%s.txt'%(KEY))
 except Exception as e:
     raise ValueError('Error while downloading temmplate file %s from %s '%(aws-batch-parameter))
-    logger.info(' ERROR while downloading template file')
+	logger.info(' ERROR while downloading template file')
 finally:
     logger.info('Downloading Template Ended')
 
-file=open('/tmp/%s'%(KEY)).read().splitlines()
+file=open('/tmp/%s.txt'%(KEY)).read().splitlines()
 print(file[count])
 
 line = file[count].split(' ')
@@ -53,11 +52,10 @@ SHOTGUN_ENTITY_TYPE=str(line[7])
 SHOTGUN_ATTRIBUTE_NAME=str(line[8])
 SHOTGUN_ATTRIBUTE_VALUE=str(line[9])
 
-PIXIT_TYPE=str(line[10])
-PIXIT_ENTITY_ID=str(line[11])
-PIXIT_ENTITY_TYPE=str(line[12])
-PIXIT_ATTRIBUTE_NAME=str(line[13])
-PIXIT_ATTRIBUTE_VALUE=str(line[14])
+PIXIT_PROJECT_TYPE=str(line[10])
+PIXIT_TYPE=str(line[11])
+PIXIT_DELIVERY_ID=str(line[12])
+PIXIT_MANIFEST_PATH=str(line[13])
 
 print(line)
 
@@ -84,7 +82,8 @@ def copy_to_vfx_vendor_test(source_bucket_key, destination_bucket_key, source_bu
         raise ValueError('Error while copying S3 objects %s from %s to %s - %s '%(source_bucket_key,source_bucket_name,destination_bucket_name,destination_bucket_key))
     finally:
         logger.info('Copying process ended')
-
+		
+		
 def send_message_to_sns(topic_arn, message_body, message_attrs):
     """
     :param topic_arn:
@@ -105,33 +104,30 @@ def main():
     :return:
     """
     copy_to_vfx_vendor_test(source_bucket_key, destination_bucket_key, source_bucket_name, destination_bucket_name)
+	
+	if count == array_size
+		
+		sleep(150);
+		
+		msg_attr={}
+		msg_body = 'Changing Delivery Status to Delivered'
+		msg_attr['Type'] = {"DataType": "String", "StringValue": SHOTGUN_TYPE}
+		msg_attr['entityId'] = {"DataType": "Number", "StringValue": SHOTGUN_ENTITY_ID}
+		msg_attr['entityType'] = {"DataType": "String", "StringValue": SHOTGUN_ENTITY_TYPE}
+		msg_attr['attributeName'] = {"DataType": "String", "StringValue": SHOTGUN_ATTRIBUTE_NAME}
+		msg_attr['attributeValue'] = {"DataType": "String", "StringValue": SHOTGUN_ATTRIBUTE_VALUE}
+	    response = send_message_to_sns(topic_arn=UAP_SNS_TOPIC, message_body=msg_body, message_attrs=msg_attr)
+        print(response)
+		
+	    sleep(150);
+		
+		msg_attr={}
+		msg_body = 'Changing Delivery Status to Delivered'
+		msg_attr['Type'] = {"DataType": "String", "StringValue": PIXIT_PROJECT_TYPE}
+        msg_attr['entityId'] = {"DataType": "Number", "StringValue": PIXIT_TYPE}
+        msg_attr['entityType'] = {"DataType": "String", "StringValue": PIXIT_DELIVERY_ID}
+        msg_attr['attributeName'] = {"DataType": "String", "StringValue": PIXIT_MANIFEST_PATH}
 
-    if count == array_size:
-
-                sleep(150);
-
-                msg_attr={}
-                msg_body = 'Changing Delivery Status to Delivered'
-                msg_attr['Type'] = {"DataType": "String", "StringValue": SHOTGUN_TYPE}
-                msg_attr['entityId'] = {"DataType": "Number", "StringValue": SHOTGUN_ENTITY_ID}
-                msg_attr['entityType'] = {"DataType": "String", "StringValue": SHOTGUN_ENTITY_TYPE}
-                msg_attr['attributeName'] = {"DataType": "String", "StringValue": SHOTGUN_ATTRIBUTE_NAME}
-                msg_attr['attributeValue'] = {"DataType": "String", "StringValue": SHOTGUN_ATTRIBUTE_VALUE}
-                response = send_message_to_sns(topic_arn=UAP_SNS_TOPIC, message_body=msg_body, message_attrs=msg_attr)
-                print(response)
-
-                sleep(150);
-
-                msg_attr={}
-                msg_body = 'Changing Delivery Status to Delivered'
-                msg_attr['Type'] = {"DataType": "String", "StringValue": PIXIT_TYPE}
-                msg_attr['entityId'] = {"DataType": "Number", "StringValue": PIXIT_ENTITY_ID}
-                msg_attr['entityType'] = {"DataType": "String", "StringValue": PIXIT_ENTITY_TYPE}
-                msg_attr['attributeName'] = {"DataType": "String", "StringValue": PIXIT_ATTRIBUTE_NAME}
-                response = send_message_to_sns(topic_arn=UAP_SNS_TOPIC, message_body=msg_body, message_attrs=msg_attr)
-                print(response)
-
-
+				
 if __name__ == '__main__':
     main()
-                                                             
